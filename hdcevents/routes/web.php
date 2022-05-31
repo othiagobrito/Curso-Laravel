fbaa863e8fdcfea720bc0c0395b2273bcc082e1f
thiagobrito <thiago7brito@gmail.com>
2022-05-31 11:43:20 -0300

    Conhecendo o blade - #04

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Thiago";
+    $idade = 24;
+
+    return view('welcome', ["nome" => $nome, "idade" => $idade, "profissao" => "programador"]);
 });
 
 Route::get('/contact', function () {
