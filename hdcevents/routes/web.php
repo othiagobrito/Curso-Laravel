81ecf2cfaa680a4ee5b80dea48ef1d3ef55c6400
thiagobrito <thiago7brito@gmail.com>
2022-05-31 11:30:14 -0300

    Rotas e Views - #03

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
+
