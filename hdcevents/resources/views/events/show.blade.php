8b9f675bceac8ec6a85bdd69cee09955dbe45e97
thiagobrito <thiago7brito@gmail.com>
2022-06-01 20:38:04 -0300

    Salvando JSON no banco de dados - #19

--- hdcevents/resources/views/events/show.blade.php
+++ hdcevents/resources/views/events/show.blade.php
@@ -18,6 +18,13 @@
                 <p class="events-owner"><ion-icon name="star-outline"></ion-icon> Dono do evento</p>
 
                 <a href="#" class="btn btn-primary" id="event-submit">Confirmar presença</a>
+
+                <h3>O evento conta com:</h3>
+                <ul id="items-list">
+                    @foreach ($event->items as $item)
+                        <li><ion-icon name="play-outline"></ion-icon> <span>{{ $item }}</span></li>
+                    @endforeach
+                </ul>
             </div>
 
             <div class="col-md-12" id="description-container">
