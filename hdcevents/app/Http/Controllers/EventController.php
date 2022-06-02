3f20e1c697c8f013f108538c43d2d2f11084e8e8
thiagobrito <thiago7brito@gmail.com>
2022-06-01 21:36:41 -0300

    Busca no Laravel - #21

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -9,9 +9,17 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        $events = Event::all();
+        $search = request('search');
+
+        if ($search) {
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
     
-        return view('welcome', ['events' => $events]);   
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create() {
--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -6,14 +6,19 @@
 
     <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-        <form action="">
+        <form action="/" method="GET">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
         </form>
     </div>
 
     <div id="events-container" class="col-md-12">
-        <h2>Próximos eventos</h2>
-        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        @if ($search)
+            <h2>Buscando por: {{ $search }}</h2>
+        @else
+            <h2>Próximos eventos</h2>
+            <p class="subtitle">Veja os eventos dos próximos dias</p>
+        @endif
+
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
@@ -27,8 +32,10 @@
                 </div>
             @endforeach
 
-            @if (count($events) == 0)
-                <p>Não há eventos disponíveis</p>                
+            @if (count($events) == 0 && $search)
+                <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos!</a></p>
+            @elseif (count($events) == 0)
+                <p>Não há eventos disponíveis</p>
             @endif
         </div>
     </div>
